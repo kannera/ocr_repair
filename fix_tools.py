651cdb4a34dd181a89f5f8f60efce61866e90212
Kanner Antti O <kanner@lx5-sugl2.helsinki.fi>
2015-11-11 13:30:21 +0200

    first commit

---
+++ fix_tools.py
@@ -0,0 +1,226 @@
+import json
+import re
+import operator
+
+INPUT = 0
+OUTPUT = 1
+
+START = 0
+MIDDLE = 1
+END = 2
+
+
+
+def parse_data_row(line):
+    
+    line = re.split("\t", line)[1:]
+    l = int(len(line)/2)
+    return  [line[i*2:i*2+2] for i in range(0, l)]
+
+def parse_corrections_to_list(corrections):
+
+    correction_list = []
+
+    for row in corrections:
+
+        for pair in row:
+
+            correction_list.append(pair)
+
+    return correction_list
+
+def character_combinations(count, word):
+
+    return [[word[0:i], word[i:i+count], word[i+count:len(word)]] for i in range(0, len(word)-count+1)]
+    
+def get_trigram_factor(fragment, table):
+
+#calculates the probability of a given fragments last three characters in the table of trigrams
+    
+    if len(fragment) == 1: fragment = "##"+fragment
+    elif len(fragment) == 2: fragment = "#"+fragment
+    else: fragment = fragment[-3:]
+    
+    if fragment in table: return table[fragment]
+    else: return table["nonce"]
+   
+def get_character_frequency_table(word_list):
+
+#combines a character frequency list from list of corrected words
+#{c1 : n1, c2 : n2}
+
+   
+    frequency_list = dict()
+
+    for word in word_list:
+
+        combinations = []
+
+        for i in range(1, 3):
+
+            combinations.extend(character_combinations(i, word))
+
+        for c in combinations:
+
+            character = c[MIDDLE]
+            
+            if character in frequency_list: frequency_list[character] += 1
+            else: frequency_list.update( { character : 1 } )
+
+    return frequency_list
+
+
+def build_replacement_probability_table(correction_list, character_frequency_list):
+
+#return a dict of dicts, where keys are characters and values are key-value pairs of characters possible replacements and their respective probabilities
+
+    table = { x : { x : character_frequency_list[x] } for x in character_frequency_list }
+    
+    for pair in correction_list:
+        if pair[INPUT] in table:
+            table[pair[INPUT]][pair[INPUT]] -= 1
+            if pair[OUTPUT] in table[pair[INPUT]]: table[pair[INPUT]][pair[OUTPUT]] += 1
+            else: table[pair[INPUT]].update( { pair[OUTPUT] : 1 })
+        else:
+            table.update( { pair[INPUT] : { pair[INPUT] : 0, pair[OUTPUT] : 1 } } )
+            
+    for row in table:
+        row_sum = sum(table[row].values())
+        for col in table[row]: 
+            table[row][col] = table[row][col] / row_sum
+            
+    for row in table:
+        if len(row) > 1:
+            for col in table[row]:
+                factor=1
+                for r in row:
+                    factor = factor*table[r][r]
+                table[row][col] = table[row][col]*(1-factor)
+
+    unknown_c = "abcdefghijklmnopqrstuvxyzäö"
+
+    unknown = { c : 1/(len(unknown_c)+1) for c in unknown_c }
+
+    table.update( { "unknown" : unknown })
+    table["unknown"].update( { "unknown" : 1/len(unknown)+1 } )
+
+    return table
+        
+
+def add_split_marks(fragment, count):
+
+    for i in range(1, count): fragment += "<+>"
+
+    return fragment
+                     
+def get_split_list(correction_list):
+
+    return [x for x in correction_list if len(x[INPUT]) > 1]
+
+        
+def get_word_combinations(word):
+
+    combinations = []
+    for i in range(1,4):
+        combinations.extend(character_combinations(i, word))
+
+    return combinations
+
+def check_split_list(split_list, fragment):
+    return [x[INPUT] for x in split_list if fragment.startswith(x[INPUT])]
+
+
+def run_through_matrix(word, correction_matrix, table):
+    guesses = dict()
+    fragments = dict()
+    for i in range(0, len(correction_matrix)):
+        new_fragments = []
+
+        for j in correction_matrix[i]:
+            j_sub = j
+            j_prob = correction_matrix[i][j]
+            if len(fragments) > 0:
+                for f in fragments:
+                    if f["fragment"].endswith("<+>"):
+                        
+                        new_fragments.append( { "fragment" : re.sub("<\+>", "", f["fragment"], count=1), "prob" : f["prob"] } )
+
+                    else:
+                        
+                        k = f["fragment"]
+                   
+                        nf = k+j_sub
+                        
+                        trigram_factor = get_trigram_factor(nf, table)
+                        prob = f["prob"]*j_prob*trigram_factor
+                        new_fragments.append( { "fragment" : nf ,  "prob" : prob}  )
+                        
+
+            else:
+                trigram_factor = get_trigram_factor(j_sub, table)
+                new_fragments.append( { "fragment" : j_sub,  "prob" : j_prob*trigram_factor }  )
+
+        new_fragments = get_top_100_fragments(new_fragments)
+        fragments = new_fragments
+
+   
+
+    return fragments
+
+
+def get_word_probability(word):
+
+    corpus_sizes = { "182x" : {"tokens" : 575179,  "types"  : 65941 },
+                     "183x" : {"tokens" : 1377160, "types"  : 128880 },
+                     "184x" : {"tokens"  : 2998726, "types" : 197429 },
+                     "185x" : {"tokens"  : 17038824,"types" : 525143 },
+                     "186x" : {"tokens"  : 37430663, "types": 916087 },  
+                     "187x" : {"tokens"  : 79244434, "types": 1413128 }, 
+                     "188x" : {"tokens"  : 276140381, "types": 2824262 }, 
+                     "189x" : {"tokens"  : 732014562, "types": 4849579 }}
+
+    freqs = []    
+    for corpus in corpus_sizes:
+        
+        with open("resources/grams/OF_klk_fi_1grams_"+corpus+"-20140905", "r", encoding="utf-8" ) as f:
+            stop = False
+            freq = run_list(f, word)
+
+        freq = freq/(corpus_sizes[corpus]["types"]+corpus_sizes[corpus]["tokens"])
+        freqs.append(freq)
+    return sum(freqs)/len(freqs)
+
+def run_list(f, word):
+
+    for line in f:
+        w = re.split("\t", line)
+        if word == w[0]: return int(w[1])+1
+
+    return 1
+
+
+def get_new_frag(pos):
+    
+    z = pos["sub"]
+    for j in range(1, len(pos["orig"])): z += "<+>"
+    return z
+
+
+def get_top_100_fragments(fragments):
+    
+    filtered_fragments = []
+    for x in fragments:
+        if x not in filtered_fragments: filtered_fragments.append(x)
+
+
+    fragments = filtered_fragments
+
+    sorted_fragments = sorted(fragments, key=lambda k: k["prob"], reverse=True)
+    if len(sorted_fragments) > 100: 
+
+        return sorted_fragments[:100]
+    else: 
+        
+        return sorted_fragments
+
+
---
+++ ocr_repair.py
@@ -0,0 +1,100 @@
+import json
+from fix_tools import *
+import re
+import operator
+
+with open("resources/testword_list.txt", "r", encoding="utf-8") as f:
+    testword_list = [ re.sub("\n", "", x) for x in f]
+
+with open("resources/fix_data.tsv", "r", encoding="utf-8") as f:
+    corrections = [parse_data_row(re.sub("\n", "", x)) for x in f]
+
+with open("resources/test_list.txt", "r", encoding="utf-8") as f:
+    test_list = [re.sub("\n", "", x) for x in f]
+
+with open("resources/trigram_list.json", "r", encoding="utf-8") as f:
+    trigram_table = json.load(f)
+
+# necessary resources are built here.
+
+correction_list = parse_corrections_to_list(corrections)
+split_list = get_split_list(correction_list)
+character_frequency_table = get_character_frequency_table(testword_list)
+replacement_probabilities = build_replacement_probability_table(correction_list, character_frequency_table)
+
+def analyse_word(word):
+# the word in iterated character by character and a matrix is built based on each possible replacement character for each character in the word. Also splitting characters are taken in to account.
+# the matrix is passed through to run_through_matrix(), which calculates individual probabilities of each possible route through the matrix. Only 100 most probable possibilities are chosen to be analysed for the next step through the matrix.
+
+    correction_matrix = []
+    
+    for i in range(0, len(word)):
+
+        c = word[i]
+        if c not in replacement_probabilities: c = "unknown" 
+       
+        if len(replacement_probabilities[c]) > 1:
+            possibilities = [c]
+            split_pos = check_split_list(split_list, word[i:])        
+            possibilities.extend(split_pos)
+            row = dict()
+            factor = 1
+            for pos in possibilities:
+
+                if len(pos) > 1: 
+                    factor = replacement_probabilities[pos][pos]
+
+            for pos in possibilities:
+                   	
+                        
+                for rep in replacement_probabilities[pos]: 
+                    rep_s = add_split_marks(rep, len(pos))
+                    if len(pos) == 1: f = factor
+                    else: f = 1
+                    if len(pos) > 1 and rep == pos:
+                        that = True
+                    else:row.update({ rep_s : replacement_probabilities[pos][rep]*f })
+           
+            correction_matrix.append(row)
+            
+        else:
+            correction_matrix.append( { c : 1 } )
+      
+    guesses = run_through_matrix(word, correction_matrix, trigram_table)
+
+    return guesses        
+ 
+
+def calculate_final_probabilities(guesses):
+
+    probabilities = dict()
+    for guess in guesses:
+        factor = get_word_probability(guess["fragment"])
+        probabilities.update( { guess["fragment"] : guess["prob"]*factor } )
+
+    return probabilities
+
+def analyse(word, n=3):
+# word is the word to be analysed
+# n is the length of the list of most probable outcomes returned
+# analyse_word() returns 100 most probable guesses, calculate_final_probabilities() assigns them new probabilities taking account the outputs probability in a wordlist.
+
+    guesses = analyse_word(word)
+    guesses = calculate_final_probabilities(guesses)
+    guesses = sorted(guesses.items(), key=operator.itemgetter(1), reverse=True)
+    
+    return guesses[0:int(n)]
+
+
+#        if i < 11 :print(word, guesses[i])
+#        if guesses[i][0] == "maksakoon": 
+#            print(i, word, guesses[i])
+#            maksakoon = guesses[i]
+        
+        
+#print(word_probability_table["kostaaksensa"]*maksakoon[1]/word_probability_table["nonce"])          
+#print(correction_probabilities["f"])
+#print(scramble_probabilities["f"])
+
+
+
